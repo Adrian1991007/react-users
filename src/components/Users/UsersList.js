c6f171645f32941795ef5297092d4494b50a7b2b
Gherasim Daniel Adrian <adriangherasim1@gmail.com>
2021-11-17 16:42:35 +0200

    working with ref's

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,22 +1,19 @@
 import React from "react";
+
 import Card from "../UI/Card";
 import styles from "./UsersList.module.css";
 
 const UsersList = (props) => {
   return (
-    <div>
-      {props.empty === false && (
-        <Card className={styles.users}>
-          <ul>
-            {props.users.map((user) => (
-              <li key={user.id}>
-                {user.name} ({user.age} years old)
-              </li>
-            ))}
-          </ul>
-        </Card>
-      )}
-    </div>
+    <Card className={styles.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
   );
 };
 
